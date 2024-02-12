1b2564913986ab1dbe9226804c51f38148866e84
Amitabhsar <148541062+Amitabhsar@users.noreply.github.com>
2024-02-12 20:16:03 +0200

    Add files via upload

---
+++ MainActivity (3).kt
@@ -0,0 +1,26 @@
+package com.coding.meet.photopickerapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val imageBtn = findViewById<Button>(R.id.imagePickBtn)
+        imageBtn.setOnClickListener {
+            startActivity(Intent(this,ImageActivity::class.java))
+        }
+
+
+        }
+
+
+
+
+
+
+    }
